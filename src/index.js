c3300be8a26741c3418a2b61bf64c1024f42bf07
Roman Lysov <rmlysov@gmail.com>
2019-02-05 14:14:05 +0300

    babel, html, case, react packages installed and configured

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import _ from 'lodash';
 function component() {
     let element = document.createElement('div');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', 'Roman'], ' ');
 
     return element;
 }
 
 document.body.appendChild(component());
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,39 @@
 const path = require('path');
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
+    context: path.join(__dirname, 'src'),
+    resolve: {
+        extensions: [".js", ".jsx"]
+    },
+
+    entry: './index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
+    plugins: [
+        new CaseSensitivePathsPlugin(),
+        new HtmlWebpackPlugin({
+            title: "Hello Roman",
+            hash: true,
+            template: "./index.html"
+        })
+        // other plugins ...
+    ]
 };
