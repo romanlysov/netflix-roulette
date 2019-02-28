1f81535ead7fea3eb68fa3735f83c6980271cbd3
Roman Lysov <rmlysov@gmail.com>
2019-02-28 18:51:35 +0300

    fetchFilms refactored

--- src/components/AsyncApp/spec.js
+++
@@ -1,6 +0,0 @@
-import { getFilms } from 'components/AsyncApp/index'
-
-test('calls axios', async () => {
-    const films = await getFilms({sortBy: 'release_date', value: 'terminator',searchByFilter: 'title'})
-    expect(films.length).toEqual(10)
-})
--- src/components/FetchFilms/index.jsx
+++ src/components/FetchFilms/index.jsx
@@ -1,10 +1,6 @@
 import { actionCreator } from '../../actions'
-import { SearchStatus } from '../../constants'
 
 export const fetchFilms = async (dispatch, fetch) => {
   const films = await fetch()
-  dispatch(actionCreator.setFilmsLoadingStatus(true))
-  dispatch(actionCreator.getFilmsData(films))
-  dispatch(actionCreator.setMainView(SearchStatus.showRequested))
-  dispatch(actionCreator.updateMoviesFoundQuantity(films.length))
+  dispatch(actionCreator.setAllFilmsInfo(films))
 }
